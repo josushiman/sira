6b19ece43e74759b8fc9d4718aab8a8d75a84f11
TimMus <timurmustafa1989@gmail.com>
2026-08-18 12:37:49 +0100

    first commit

---
+++ sira/siraApp.swift
@@ -0,0 +1,17 @@
+//
+//  siraApp.swift
+//  sira
+//
+//  Created by Timur Mustafa on 18/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct siraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
